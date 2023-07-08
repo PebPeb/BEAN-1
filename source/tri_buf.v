02c69060e1ed4550153f7f797cdff5bd0a96708c
PebPeb <67885698+PebPeb@users.noreply.github.com>
2023-07-08 17:59:21 -0400

    Initial Commit

---
+++ source/tri_buf.v
@@ -0,0 +1,25 @@
+
+//
+//	tri.v
+//		Adjustable data width Tri State Buffer
+//
+
+// -------------------------------- //
+//	By: Bryce Keen	
+//	Created: 06/30/2023
+// -------------------------------- //
+//	Last Modified: 06/30/2023
+
+// Change Log:	NA
+
+
+module tri_buf(data_in, data_out, enable);
+	parameter WIDTH = 32;
+	
+	input wire [WIDTH - 1:0]	data_in;
+    input wire      			enable;
+	inout wire [WIDTH - 1:0]	data_out;
+
+    assign data_out = enable ? data_in : {WIDTH{1'bZ}};
+
+endmodule
